371855615d557cf4352ae97bf87f51a8c57d099b
Wenjing Yang <3190103326@zju.edu.cn>
2022-11-07 00:50:54 +0800

    add

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.io import imread
-from skimage.filters import median
+from skimage.filters import _median
 
 TB = imread("Tuberculosis.png")
-plt.imshow(TB, cmap = "gray")
-plt.show()
+
+
